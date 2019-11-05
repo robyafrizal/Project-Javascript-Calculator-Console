02bc8ab7150a5d3eeb68bd8c5f8f2b9e9f58a4d8
robyafrizal <robyafrizal86@gmail.com>
2019-11-05 15:38:01 +0700

    Add Project Javascript Calculator Console

---
+++ index.js
@@ -0,0 +1,12 @@
+const digitA = prompt("Masukkan Angka pertama");
+const digitB = prompt("Masukkan angka kedua");
+
+const resultAddition = digitA + digitB;
+const resultSubstraction = digitA - digitB;
+const resultMultiplication = digitA * digitB;
+const resultDivision = digitA / digitB;
+
+console.log(`Angka ${digitA} + ${digitB} adalah ${resultAddition}`);
+console.log(`Angka ${digitA} - ${digitB} adalah ${resultSubstraction}`);
+console.log(`Angka ${digitA} x ${digitB} adalah ${resultMultiplication}`);
+console.log(`Angka ${digitA} : ${digitB} adalah ${resultDivision}`);
